e7db54c18c38b67fcfdffd0c1c654fa7d4079acc
cnt000 <cnt000@users.noreply.github.com>
2022-04-17 19:37:21 +0000

    deploy: ffacdc3df43cb6589d12eed31c041b0fed4346c4

---
+++ precache-manifest.33ba65f9f123f9198bc7b773767ad78f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a769b411921779d9038122458c52e416",
+    "url": "https://cnt000.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "3a8d723fad44b18ef8ad",
+    "url": "https://cnt000.github.io/task-management-frontend/static/js/2.9c67c15d.chunk.js"
+  },
+  {
+    "revision": "f88dcd2c0a2d3314e7fc",
+    "url": "https://cnt000.github.io/task-management-frontend/static/js/main.21fb4306.chunk.js"
+  },
+  {
+    "revision": "b108ebf525592840aeef",
+    "url": "https://cnt000.github.io/task-management-frontend/static/js/runtime~main.45df27e4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://cnt000.github.io/task-management-frontend/precache-manifest.33ba65f9f123f9198bc7b773767ad78f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://cnt000.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
